01774eabda57d3c88501201ef31b86d94ec8d542
Priyanshu Garg <92843561+Priyans-hu@users.noreply.github.com>
2026-01-29 21:37:57 +0530

    chore: bump argus to v0.4.0

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -1,26 +1,26 @@
 class Argus < Formula
   desc "Auto-generate AI context files from your codebase"
   homepage "https://github.com/Priyans-hu/argus"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Priyans-hu/argus/releases/download/v#{version}/argus_#{version}_darwin_arm64.tar.gz"
-      sha256 "9e87f2bedc18a575787eda23dd4fbd09f7d20ff13b45ab2e0489b1786e51f2c9"
+      sha256 "69aa4bec0140b1f9d79fe8c64887d6cb7fad228cd3fc0e1420269e7b35f5bb1d"
     else
       url "https://github.com/Priyans-hu/argus/releases/download/v#{version}/argus_#{version}_darwin_amd64.tar.gz"
-      sha256 "3df51f53a94cf78b90a009733ad9b138dfb834d8226984eccb1e35850f8e2a4c"
+      sha256 "e42b1b85162062beca777fd77d2acd783f798d1ab5e042b3c15771a87b3b078c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Priyans-hu/argus/releases/download/v#{version}/argus_#{version}_linux_arm64.tar.gz"
-      sha256 "a6bfc5bb056d81e04cf224f95bb3f73cda0f6affb1b334132bb99504f146d45b"
+      sha256 "ab88875df36543a9c920a181133a4d35ab324d1a3e0142f97bcc8c801aef4431"
     else
       url "https://github.com/Priyans-hu/argus/releases/download/v#{version}/argus_#{version}_linux_amd64.tar.gz"
-      sha256 "3d25c2b03a9e94919833865040e66e5c7baafa25a9a7b2a1ea7f6c250ca45d44"
+      sha256 "36081002dded840c27306ec6154e78858d36f062c8ac48619f59af10a1bd1438"
     end
   end
 
